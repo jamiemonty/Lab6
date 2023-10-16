c4510ec7a437758ced453568bdcd45363fedac87
Jamie <G00419525@atu.ie>
2023-10-16 17:41:55 +0100

    Added a part to add two numbers

--- OOP/Lab4.1/untitled/src/main/java/ie/atu/Calculator.java
+++ OOP/Lab4.1/untitled/src/main/java/ie/atu/Calculator.java
@@ -1,4 +1,28 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+    public static void main(String[] args) {
+        add();
+    }
+
+    public static void add()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        Scanner inputs = new Scanner(System.in);
+        int secondNumber = inputs.nextInt();
+
+        System.out.println("The total is " + total);
+    }
+
+    public static void delete()
+    {
+
+    }
 }
